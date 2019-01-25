1127ac7f656cf632bbb9af2fc909800923e892eb
Joe Ganobsik <jganobsik@gmail.com>
2019-01-25 19:57:16 +0000

    Automatically backed up by Learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,5 +1,19 @@
-class Ship < ActiveRecord::Base
-  belongs_to :pirate
+class Ship
+  attr_reader :name, :type, :booty
+
+  @@ships = []
+
+  def initialize(args)
+    @name = args[:name]
+    @type = args[:type]
+    @booty = args[:booty]
+    @@ships << self
+  end
+
+  def self.all
+    @@ships
+  end
+
   def self.clear
     @@ships = []
   end
